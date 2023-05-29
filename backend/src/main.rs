b4673cf0ce5e083816397c6dcd306a9eebbf8537
Lucas <keyruu@web.de>
2023-05-29 16:43:57 +0200

    🌱 (backend): add basic graphql schema

---
+++ backend/src/main.rs
@@ -0,0 +1,46 @@
+use async_graphql::{
+    http::GraphiQLSource, Context, EmptyMutation, EmptySubscription, Object, Schema,
+};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::{
+    extract::Extension,
+    response::{self, IntoResponse},
+    routing::get,
+    Router, Server,
+};
+
+async fn graphql_handler(
+    schema: Extension<Schema<QueryRoot, EmptyMutation, EmptySubscription>>,
+    req: GraphQLRequest,
+) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn graphiql() -> impl IntoResponse {
+    response::Html(GraphiQLSource::build().endpoint("/").finish())
+}
+
+pub struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn books(&self, ctx: &Context<'_>) -> String {
+        return "book".to_string();
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+
+    let app = Router::new()
+        .route("/", get(graphiql).post(graphql_handler))
+        .layer(Extension(schema));
+
+    println!("GraphiQL IDE: http://localhost:8000");
+
+    Server::bind(&"127.0.0.1:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
